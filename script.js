911d1b04f78a47cf6e051d6060dfd053fa7a7703
Your Name <you@example.com>
2024-12-01 18:15:37 -0800

    added a thing that displays who's turn it is

--- script.js
+++ script.js
@@ -16,7 +16,15 @@ class TicTakToe {
         [2, 5, 8],//right column
         [0, 4, 8],//diagonal top left to middle right
         [2, 4, 6],//diagonal top right to bottom left
-    ]
+    ];
+
+    // Get DOM elements for UI updates
+    this.turnDisplay = document.getElementById('turn'); // Display the current player's turn
+    this.scoreboardX = document.getElementById('scoreboard-x'); // Display the score of 'X'
+    this.scoreboardO = document.getElementById('scoreboard-o'); // Display the score of 'O'
+    this.playAgainBtn = document.getElementById('button-play-again'); // Button to reset the game
+    this.gameBoard = document.getElementById('game-board'); // The game board
+
 
     }
 }
